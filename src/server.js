df634889e5921bf4b6c508735f589a8c9776cc97
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-24 15:04:41 +0200

    set up welcome route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
+const serverMiddleware = require('./middleware/serverMiddleware');
+const rootRoute = require('./controllers/rootController');
 const server = express();
 
+serverMiddleware(server);
+server.get('/', rootRoute);
 module.exports = server;
