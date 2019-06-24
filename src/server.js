afb4d64af2e5860b918b4f789a8ec28fd64b8c30
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-24 15:33:59 +0200

    set up routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const serverMiddleware = require('./middleware/serverMiddleware');
 const rootRoute = require('./controllers/rootController');
+const router = require('./routes');
 const server = express();
 
 serverMiddleware(server);
 server.get('/', rootRoute);
+server.use('/api', router);
+
 module.exports = server;
