7bb30c97b9021c0f456bd1536cdca1587461f75b
Ivana Huckova <ivana.huckova@gmail.com>
2019-06-25 14:31:09 +0200

    update and delete user functionality

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -3,5 +3,7 @@ const usersController = require('../controllers/usersController');
 const authentication = require('../middleware/authMiddleware');
 
 router.route('/profile').get(authentication, usersController.getUser);
+router.route('/profile').put(authentication, usersController.updateUser);
+router.route('/profile').delete(authentication, usersController.deleteUser);
 
 module.exports = router;
