c0d2bf85262168df945b2c666d7376b57d3c4475
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-01 20:47:25 +0200

    api route test

---
+++ src/routes/apiRoute.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('../server');
+
+describe('API route', () => {
+    it('should return 200 on success', async () => {
+        const res = await request(server).get('/api');
+        expect(res.status).toEqual(200);
+    });
+});
